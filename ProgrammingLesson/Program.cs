e3c5c83ee8e9a35e70a55d7abb496c5916b5b948
ITUpskilling <testforquality@hotmail.com>
2019-09-28 14:00:13 +0100

    First class in Programming

--- ProgrammingLesson/Program.cs
+++ ProgrammingLesson/Program.cs
@@ -8,8 +8,135 @@ namespace ProgrammingLesson
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //an entry point
         {
+            //Must learn C-sharp keywords are
+            //Class (instance, static)
+            //Class Constructor, Object
+            //Method / Function (void, type (return), argument/parameter)
+            //Datatypes (string (covered), integer (covered), boolean , var, double (covered) etc)
+            //Access modifier (private, public etc)
+            //String manipulation, String concatenation (covered)
+            //Operators (+ (covered), - (covered), * (covered), / (covered), % (covered))
+            //Comparison operators (== (covered), <=, >=, !=, &&, || etc)
+            //Control statements (Selection and Iteration)
+            //Selection statements (if (covered), switch-case)
+            //Iteration statements (for, while, foreach)
+            //Collections (List, Dictionary, Array, ArrayList)
+            //Object Oriented Programming Concepts <OOP> 
+            //OOP (Encapsulation, Polymorphism, Inheritance, Abstraction)
+            //Read and write of data to a file (Notepad) and SQL Server Database
+            //Debugging
+            //Try-catch (error or exception handling)
+            //Code refactoring
+
+            ////Explanation 
+            //int age = 55;  //int = datatype, age = variable, 55 = value or data
+            //string firstName = "Becky"; //string = datatype, firstName = variable, Becky = value or data
+            //int firstName2 = Convert.ToInt16("Ola");
+
+
+            ////Task 1
+            ////Write a computer program to add two numbers. Display the result 
+            ////to the console
+            ////Hint: you will need the followings
+            ////(1) 3 variables which can be 'firstNumber', 'secondNumber', 'answer'
+            ////(2) Datatype(s) for each variable. An integer type for all variables
+            ////(3) Operator addition '+' and an assignment symbol which is '='
+
+            //Console.WriteLine("Computer program to add two numbers");
+            //Console.WriteLine("What is the first integer number to add");
+            //int firstNumber = Convert.ToInt16(Console.ReadLine());  //Console.ReadLine() means get an input from a user at runtime
+            //Console.WriteLine("What is the second integer number to add");
+            //int secondNumber = Convert.ToInt16(Console.ReadLine());
+            //int answer = firstNumber + secondNumber;
+            //Console.WriteLine("Addition of two numbers = " + answer);              //Console.WriteLine(answer) means display an answer to the console //Concatenation
+
+
+            ////Task 2
+            ////Write a computer program to add two numbers of any type (decimal or integer). 
+            //// Display the result to the console
+            ////Hint: you will need the followings
+            ////(1) 3 variables which can be 'firstNumber', 'secondNumber', 'answer'
+            ////(2) Datatype(s) for each variable. A double type for all variables
+            ////(3) Operator addition '+' and an assignment symbol which is '='
+            ////
+            //Console.WriteLine("Computer program to add two numbers");
+            //Console.WriteLine("What is the first number to add");
+            //double firstNumber = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("What is the second number to add");
+            //double secondNumber = Convert.ToDouble(Console.ReadLine());
+            //double answer = firstNumber + secondNumber;
+            //Console.WriteLine($"Addition of {firstNumber} and {secondNumber} = {answer}");
+
+            ////Task 3
+            ////Write a computer program to multiply three numbers of any type (decimal or integer). 
+            //// Display the result to the console
+            ////Hint: you will need the followings
+            ////(1) 4 variables which can be 'firstNumber', 'secondNumber', 'thirdNumber', 'answer'
+            ////(2) Datatype(s) for each variable. A double type for all variables
+            ////(3) Operator multiplication '*' and an assignment symbol which is '='
+            ////
+            //Console.WriteLine("Computer program to multiply three numbers");
+            //Console.WriteLine("Input the first number to multiply and press enter");
+            //double firstNumber = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Input the second number to multiply and press enter");
+            //double secondNumber = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Input the third number to multiply and press enter");
+            //double thirdNumber = Convert.ToDouble(Console.ReadLine());
+            //double answer = firstNumber * secondNumber * thirdNumber;
+            //Console.WriteLine($"Multiplication of {firstNumber} , {secondNumber} and the {thirdNumber} = {answer}");
+
+            ////Task 4
+            ////Write a computer program to divide two numbers of any type (decimal or integer). 
+            //// Display the result to the console
+            ////Hint: you will need the followings
+            ////(1) 3 variables which can be 'firstNumber', 'secondNumber', 'answer'
+            ////(2) Datatype(s) for each variable. A double type for all variables
+            ////(3) Operator division '/' and an assignment symbol which is '='
+            ////
+            //Console.WriteLine("Computer program to divide two numbers");
+            //Console.WriteLine("Input the first number to divide and press enter");
+            //double firstNumber = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Input the secondNumber number to divide and press enter");
+            //double secondNumber = Convert.ToDouble(Console.ReadLine());
+            //double answer = firstNumber / secondNumber;
+            //Console.WriteLine($"Division of {firstNumber} and {secondNumber} = {answer}");
+
+            ////Task 5
+            ////Write a computer program to find a remainder of division of two numbers 
+            //// Display the result to the console
+            ////Hint: you will need the followings
+            ////(1) 3 variables which can be 'firstNumber', 'secondNumber', 'answer'
+            ////(2) Datatype(s) for each variable. A double type for all variables
+            ////(3) Operator modulo '%' and an assignment symbol which is '='
+            //// 
+            //Console.WriteLine("Computer program to find a remainder of division of two numbers");
+            //Console.WriteLine("Input the first number to find a remainder for and press enter");
+            //double firstNumber = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Input the secondNumber number to find a remainder for and press enter");
+            //double secondNumber = Convert.ToDouble(Console.ReadLine());
+            //double answer = firstNumber % secondNumber;
+            //Console.WriteLine($"Remainder of {firstNumber} and {secondNumber} = {answer}");
+
+            ////Task 6
+            ////If-statement              
+            ////To get a code snippet, type the first keyword and press 'Tab' key twice
+            ////Write a computer program to ask a user to input a whole number. Then tell a 
+            ////user if the inputted number is an 'even' or 'odd' number by displaying it to console
+            ////An even number is a number that is divisible by 2 with remainder 0
+            ////An odd number is a number that is divisible by 2 with remainder 1
+            //Console.WriteLine("Input an integer number and press enter");
+            //int number = Convert.ToInt16(Console.ReadLine());
+            //if (number % 2 == 0)  //this is to test for an even number
+            //{
+            //    Console.WriteLine($"{number} is an even number");
+            //}
+            //else if (number % 2 == 1)  //this is to test for an odd number
+            //{
+            //    Console.WriteLine($"{number} is an odd number");
+            //}
+
         }
     }
 }
