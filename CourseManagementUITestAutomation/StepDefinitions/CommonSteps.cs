6d498efc68f04f85161d863c3cd990c4efd80999
ITUpskilling <testforquality@hotmail.com>
2019-10-20 14:40:44 +0100

    Extent Report Implementation

--- CourseManagementUITestAutomation/StepDefinitions/CommonSteps.cs
+++ CourseManagementUITestAutomation/StepDefinitions/CommonSteps.cs
@@ -8,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
 
 namespace CourseManagementUITestAutomation.StepDefinitions
 {
@@ -15,6 +18,9 @@ namespace CourseManagementUITestAutomation.StepDefinitions
     public class CommonSteps
     {
         Context context;
+        static ExtentTest feature;
+        static ExtentTest scenario;
+        static ExtentReports report;
 
         public CommonSteps(Context _context)
         {
@@ -25,7 +31,84 @@ namespace CourseManagementUITestAutomation.StepDefinitions
         public void GivenThatCMSApplicationIsLoaded()
         {
             context.LoadCMSApplication();
+            scenario = feature.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
         }
+
+        [BeforeTestRun]
+        public static void ReportGenerator()
+        {
+            var testResultReport = new ExtentV3HtmlReporter(Directory.GetCurrentDirectory() + @"\TestResult.html");
+            testResultReport.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            report = new ExtentReports();
+            report.AttachReporter(testResultReport);
+        }
+
+        [AfterTestRun]
+        public static void ReportCleaner()
+        {
+            report.Flush();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature()
+        {
+            feature = report.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
+        }
+
+        [AfterStep]
+        public void StepsInTheReport()
+        {
+            var typeOfStep = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            //Cater for a step that passed
+            if (ScenarioContext.Current.TestError == null)
+            {
+                if (typeOfStep.Equals("Given"))
+                {
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                }
+                else if (typeOfStep.Equals("When"))
+                {
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                }
+                else if (typeOfStep.Equals("Then"))
+                {
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                }
+            }
+            //Cater for a step that failed
+            if (ScenarioContext.Current.TestError != null)
+            {
+                if (typeOfStep.Equals("Given"))
+                {
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+                }
+                else if (typeOfStep.Equals("When"))
+                {
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+                }
+                else if (typeOfStep.Equals("Then"))
+                {
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+                }
+            }
+            //Cater for a step that has not been implemented
+            if (ScenarioContext.Current.ScenarioExecutionStatus.ToString().Equals("StepDefinitionPending"))
+            {
+                if (typeOfStep.Equals("Given"))
+                {
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                }
+                else if (typeOfStep.Equals("When"))
+                {
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                }
+                else if (typeOfStep.Equals("Then"))
+                {
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                }
+            }
+        }
+
 
         [AfterScenario]
         public void CloseAUT()
